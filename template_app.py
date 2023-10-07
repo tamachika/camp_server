c389a886c6282ba959cb1cc1cd78594f15b2813b
友近武史 <tomochikatakeshi@tomochikatakeshinoMacBook-Air.local>
2023-10-07 11:24:21 +0900

    name

--- template_app.py
+++ template_app.py
@@ -8,6 +8,9 @@ def index():
     # index.htmlをレンダリングする
     return render_template("index.html")
 
-
+@app.route("/user/<name>")
+def hello(name):
+    return render_template("hello.html, name=name")
+    
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
