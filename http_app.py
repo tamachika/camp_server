c19b6725e1a3726e86ce8303dc9605f344077c4f
友近武史 <tomochikatakeshi@tomochikatakeshinoMacBook-Air.local>
2023-10-07 10:06:00 +0900

    q

--- http_app.py
+++ http_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,8 @@ app = Flask(__name__)
 # ログイン機能
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    # リクエストメソッドの使用
+    print(request.method)
     return "login page"
 
 
